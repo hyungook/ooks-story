acf220bbacd77c5c81010a0185221b2de5c8c320
오형욱 <nenonenoberneno@gmail.com>
2020-11-30 22:18:24 +0900

    오류 수정 중

--- src/components/Page__nav.js
+++ src/components/Page__nav.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 
 import { Link, Route } from 'react-router-dom';
-import Home from '../Page/Home';
+
 import Work from '../Page/Work';
 import About from '../Page/About';
 import ToyProj from '../Page/ToyProject';
@@ -42,17 +42,21 @@ class Page__nav extends React.Component {
                 onOff = true;
             }
         })
-    }
 
+        let backBtn = document.querySelector('.backward');
+        backBtn.addEventListener('click', () => {
+            window.history.back();
+        })
+
+
+    }
 
     render() {
 
         return <Section className={"header"}>
             <Header__top className={"header__top"}>
                 <Backword className={"backward"}>
-                    <Link to="/" className={"link"}>
-                        <FontAwesomeIcon icon={faAngleLeft} className={"icon"}/>
-                    </Link>
+                    <FontAwesomeIcon icon={faAngleLeft} className={"icon"}/>
                 </Backword>
             </Header__top>
             <Header__bottom className={"header__bottom"}>
@@ -64,19 +68,18 @@ class Page__nav extends React.Component {
                 <HomeMenu className={"homeMenu"}>
                     <li className={"menuLink"}>WORK</li>
                     <li className={"menuLink"}>ABOUT</li>
-                    <li className={"menuLink"}>TOY PROJ</li>
-                    {/* <li className={"menuLink"}><Link to="/work" className={"MainBtn"}>WORK</Link></li>
-                    <li className={"menuLink"}><Link to="/about" className={"MainBtn"}>ABOUT</Link></li>
-                    <li className={"menuLink"}><Link to="/ToyProject" className={"MainBtn"}>TOY PROJ</Link></li> */}
+                    <li className={"menuLink"}>TOY-PJ</li>
+                    {/* <li className={"menuLink"}><Link to="/work" className={"MainBtn"}>WORK</Link></li> */}
+                    {/* <li className={"menuLink"}><Link to="/about" className={"MainBtn"}>ABOUT</Link></li> */}
+                    {/* <li className={"menuLink"}><Link to="/ToyProject" className={"MainBtn"}>TOY-PJ</Link></li> */}
                     <li className={"menuLink"}>DESIGN</li>
                 </HomeMenu>
             </Header__bottom>
 
             <main>
-                <Route exact path="/" component={Home} />
-                {/* <Route exact path="/Work" component={Work} />
-                <Route exact path="/Aobut" component={About} />
-                <Route exact path="/ToyProject" component={ToyProj} /> */}
+                {/* <Route exact path="/Work" component={Work} /> */}
+                {/* <Route exact path="/Aobut" component={About} /> */}
+                {/* <Route exact path="/ToyProject" component={ToyProj} /> */}
             </main>
 
         </Section>
